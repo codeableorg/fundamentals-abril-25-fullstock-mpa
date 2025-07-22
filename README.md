7a863ade160f9fd770bba193ded7ab59de9be7cd
David Ortiz <david.ortiz.p@gmail.com>
2025-07-21 21:25:43 -0500

    home view added

---
+++ README.md
@@ -0,0 +1,19 @@
+# FullStock MPA
+
+## Instrucciones
+
+1. Instalar dependencias
+
+```
+npm install
+```
+
+2. Levantar el servidor
+
+```
+npm run dev
+```
+
+## Recursos
+
+- [Figma del proyecto](https://www.figma.com/design/elrVuNqnd2CKUcgC92Y1X0/FullStock?node-id=1280-716&t=fvz7b7sIQTjI9XdA-1)
--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 const express = require("express")
+const expressEjsLayouts = require("express-ejs-layouts")
 
 const app = express()
 const PORT = 3000
 
+app.use(express.static("public"))
+app.set("view engine", "ejs")
+app.use(expressEjsLayouts)
+
 app.get("/", (_, res) => {
-  res.end("Hola Codeable")
+  res.render("home")
+})
+
+app.get("/product", (_, res) => {
+  res.render("product-section")
 })
 
 app.listen(PORT, () => console.log(`Servidor FullStock escuchando en http://localhost:${PORT}`))
