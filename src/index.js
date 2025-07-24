209bf4a9dc10aa16de1364e9bde67cbffb424722
David Ortiz <david.ortiz.p@gmail.com>
2025-07-23 21:06:24 -0500

    product view added

--- src/index.js
+++ src/index.js
@@ -1,51 +1,72 @@
-const express = require("express")
-const expressEjsLayouts = require("express-ejs-layouts")
-const fs = require("fs")
-const path = require("path")
+const express = require("express");
+const expressEjsLayouts = require("express-ejs-layouts");
+const fs = require("fs");
+const path = require("path");
 
-const app = express()
-const PORT = 3000
+const app = express();
+const PORT = 3000;
 
-app.use(express.static("public"))
-app.set("view engine", "ejs")
-app.use(expressEjsLayouts)
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(expressEjsLayouts);
 
 app.get("/", (_, res) => {
-  res.render("home")
-})
+  res.render("home");
+});
 
-app.get("/product", (_, res) => {
-  res.render("product-section")
-})
+app.get("/product/:productId", (req, res) => {
+  const productId = req.params.productId;
 
-app.get("/category/:categorySlug", (req, res) => {
-  const categorySlug = req.params.categorySlug
-  const filePath = path.join(__dirname, "..", "/data", "data.json")
-
-  console.log(categorySlug) // polos
-  console.log(filePath)
-
-  // leer data.json
-  fs.readFile(filePath, 'utf-8', (err, data) => {
+  const filePath = path.join(__dirname, "..", "/data", "data.json");
+  fs.readFile(filePath, "utf-8", (err, data) => {
     if (err) {
-      console.error(err)
-      return
+      console.error(err);
+      return;
     }
 
-    const {categories, products } = JSON.parse(data)
+    const { products } = JSON.parse(data);
 
-    const categoryFound = categories.find(category => category.slug === categorySlug)
-    const categoryId = categoryFound === undefined ? -1 : categoryFound.id
-    console.log(categoryId)
+    const productFound = products.find(
+      (product) => product.id === parseInt(productId)
+    );
+
+    console.log(productFound);
+    res.render("product", {product : productFound});
+  });
+});
+
+app.get("/category/:categorySlug", (req, res) => {
+  const categorySlug = req.params.categorySlug;
+  const filePath = path.join(__dirname, "..", "/data", "data.json");
+
+  console.log(categorySlug); // polos
+  console.log(filePath);
+
+  // leer data.json
+  fs.readFile(filePath, "utf-8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    const { categories, products } = JSON.parse(data);
+
+    const categoryFound = categories.find(
+      (category) => category.slug === categorySlug
+    );
+    const categoryId = categoryFound === undefined ? -1 : categoryFound.id;
+    console.log(categoryId);
     // polos => 1
 
-    const productsBySlug = products.filter(product => product.categoryId === categoryId)
-    console.log(productsBySlug)
+    const productsBySlug = products.filter(
+      (product) => product.categoryId === categoryId
+    );
+    console.log(productsBySlug);
 
-    res.render("categories", {products: productsBySlug})
+    res.render("categories", { products: productsBySlug });
+  });
+});
 
-  })
-
-})
-
-app.listen(PORT, () => console.log(`Servidor FullStock escuchando en http://localhost:${PORT}`))
+app.listen(PORT, () =>
+  console.log(`Servidor FullStock escuchando en http://localhost:${PORT}`)
+);
